22c62abc569da098dac28fbd0b5295b66a3e4eff
husniarsyad-upi <55826563+husniarsyad-upi@users.noreply.github.com>
2020-10-12 08:21:53 +0700

    test valuex

--- app/spiders/quotes.py
+++ app/spiders/quotes.py
@@ -6,6 +6,7 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
     allowed_domains = ['news.detik.com']
     start_urls = ['https://news.detik.com/indeks']
+    valuex= 0
 
     def parse(self, response):
         self.log('I just visited: ' + response.url)
@@ -13,11 +14,20 @@ class QuotesSpider(scrapy.Spider):
             item = {
                 'media__title': quote.css('div.media__text > h3.media__title > a::text').extract_first(),
                 'media__link': quote.css('div.media__text > h3.media__title > a::attr(href)').extract_first(),
-                'media__date': quote.css('div.media__text > div.media__date > span::attr(title)').extract_first(),
+                'media__date': quote.css('div.media__text > div.media__date > span::attr(title)').extract_first()
             }
             yield item
         # follow pagination link
         next_page_url = response.xpath("//a[contains(text(),'Next')]/@href").extract_first()
         if next_page_url:
+            valuex = valuex + 1
             next_page_url = response.urljoin(next_page_url)
             yield scrapy.Request(url=next_page_url, callback=self.parse)
+
+        else
+            item = {
+                'media__title': valuex,
+                'media__link': 'done',
+                'media__date': 'done'
+            }
+            yield item
