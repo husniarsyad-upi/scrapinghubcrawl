51e6ea6d5e1645b7e741048ee082e6cb109beb3e
husniarsyad-upi <55826563+husniarsyad-upi@users.noreply.github.com>
2020-10-13 10:40:04 +0700

    corona in

--- app/spiders/quotes.py
+++ app/spiders/quotes.py
@@ -7,15 +7,27 @@ class QuotesSpider(scrapy.Spider):
     allowed_domains = ['news.detik.com']
     start_urls = ['https://news.detik.com/indeks']
 
+
     def parse(self, response):
         self.log('I just visited: ' + response.url)
         for quote in response.css('article'):
-            item = {
-                'media__title': quote.css('div.media__text > h3.media__title > a::text').extract_first(),
-                'media__link': quote.css('div.media__text > h3.media__title > a::attr(href)').extract_first(),
-                'media__date': quote.css('div.media__text > div.media__date > span::attr(title)').extract_first()
-            }
-            yield item
+            title = quote.css('div.media__text > h3.media__title > a::attr(href)').extract_first()
+            if "corona" or "covid" or "sarcov" in title.lower():
+                item = {
+                    'status' : "found", 
+                    'media__title': title,
+                    'media__link': quote.css('div.media__text > h3.media__title > a::attr(href)').extract_first(),
+                    'media__date': quote.css('div.media__text > div.media__date > span::attr(title)').extract_first()
+                }
+                yield item
+            else:
+                item = {
+                    'status' : "not found", 
+                    'media__title': title,
+                    'media__link': quote.css('div.media__text > h3.media__title > a::attr(href)').extract_first(),
+                    'media__date': quote.css('div.media__text > div.media__date > span::attr(title)').extract_first()
+                }
+                yield item
         # follow pagination link
         next_page_url = response.xpath("//a[contains(text(),'Next')]/@href").extract_first()
         if next_page_url:
